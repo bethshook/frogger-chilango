363fa0f7012ca5a25b8c131e835d29c5fd49297c
Beth Shook <beth.shook@gmail.com>
2018-06-25 19:09:20 -0500

    added new classes and methods

--- script.js
+++ script.js
@@ -9,15 +9,23 @@ var ctx = canvas.getContext('2d');
 var interval;
 var frames = 0;
 var images = {
-    xolo: './images/xoloitzcuintle.png',
-    bg: './images/cdmx3.png',
-    car: './images/taxi.png'
+    xolo: './images/xolo3.png',
+    bg: './images/cdmx4.png',
+    car: './images/taxi.png',
+    truck: './images/truck.png',
+    cyclist: './images/cyclist.png',
+    trajinera: './images/trajinera.png',
+    axolotl: './images/axolotl.png'
 }
 var sound = new Audio();
 sound.src = './audio/tamales.mp3';
 sound.loop = true;
-var cars = [];
+var taxis = [];
 var trucks = [];
+var tamaleros = [];
+var trajineras = [];
+var axolotls = [];
+var lives = 3;
 
 //classes
 class Board{
@@ -34,17 +42,22 @@ class Board{
         }.bind(this)
     }
   
-    // gameOver(){
-    //     ctx.font = "80px Avenir";
-    //     ctx.fillText("Game Over", 40,100);
-    //     ctx.font = "20px Avenir";
-    //     ctx.fillStyle = 'yellow';
-    //     var playerScore = Math.floor(frames / 60);
-    //     ctx.fillText("Press 'Esc' to reset", 170,130);
-    //     ctx.fillStyle = 'red';
-    //     ctx.font = "40px Avenir";
-    //     ctx.fillText("Player Score :" + playerScore, 120,200)
-    // }
+    gameOver(){
+        ctx.font = "120px Mexcellent-Regular";
+        ctx.fillStyle = 'white';
+        ctx.fillText("Game Over", 230,230);
+        ctx.font = "28px Courier";
+        ctx.fillStyle = 'black';
+        var playerScore = Math.floor(frames / 60);
+        ctx.fillText("Press 'Esc' to reset", 320,canvas.height-30);
+        ctx.fillStyle = 'red';
+        ctx.font = "40px Mexcellent-Regular";
+        ctx.fillText("Player Score: " + playerScore, 360,300);
+        ctx.globalAlpha = 0.5;
+        ctx.fillStyle = "gray";
+        ctx.fillRect(this.x, this.y, canvas.width, canvas.height);
+
+    }
   
     draw(){
         ctx.fillRect = this.color;
@@ -63,17 +76,18 @@ class Board{
         ctx.lineTo(canvas.width,canvas.height - 192);
         ctx.closePath();
         ctx.stroke();
-        ctx.fillStyle = '#eaf7ff';
-        ctx.fillRect = (0,110,canvas.width,256);
+        ctx.fillStyle = "#fe3f80";
+        ctx.font = '30px Courier';
+        ctx.fillText("Time: " + Math.floor(frames / 60), 30, 60 )
     }
 }
   
 class Xolo{
   constructor(){
       this.x = canvas.width / 2;
-      this.y = canvas.height - 60;
-      this.width = 58;
-      this.height = 58;
+      this.y = canvas.height - 62;
+      this.width = 62;
+      this.height = 62;
       this.image = new Image();
       this.image.src = images.xolo;
       this.image.onload = function(){
@@ -82,16 +96,12 @@ class Xolo{
 
   }
 
-//   rise(){
-//       this.y-=25;
-//   }
-
-//   isTouching(item){
-//       return  (this.x < item.x + item.width) &&
-//               (this.x + this.width > item.x) &&
-//               (this.y < item.y + item.height) &&
-//               (this.y + this.height > item.y);
-//     }
+  isTouching(item){
+      return  (this.x < item.x + item.width) &&
+              (this.x + this.width > item.x) &&
+              (this.y < item.y + item.height) &&
+              (this.y + this.height > item.y);
+    }
   
 
     draw(){
@@ -107,7 +117,7 @@ class Xolo{
     }
 
     goUp(){
-        if (this.y >= 64) this.y -=64;
+        if (this.y >= 192) this.y -=64;
     }
 
     goDown(){
@@ -117,51 +127,126 @@ class Xolo{
 }
 
 class Car{
-    constructor(){
-        this.x = canvas.width / 2;
-        this.y = canvas.height - 128;
+    constructor(x,y,img){
+        this.x = x; 
+        this.y = y; 
         this.width = 64;
         this.height = 64;
         this.image = new Image();
-        this.image.src = images.car;
+        this.image.src = img;
+        this.image.onload = function(){
+            this.draw();
+        }.bind(this);
+  
+    }
+    
+      draw(){
+        this.x-=2;
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+      }
+  }
+
+
+  class Truck{
+    constructor(x,y,img){
+        this.x = x; 
+        this.y = y; 
+        this.width = 64;
+        this.height = 64;
+        this.image = new Image();
+        this.image.src = img;
         this.image.onload = function(){
             this.draw();
         }.bind(this);
   
     }
   
-  //   rise(){
-  //       this.y-=25;
-  //   }
-  
-  //   isTouching(item){
-  //       return  (this.x < item.x + item.width) &&
-  //               (this.x + this.width > item.x) &&
-  //               (this.y < item.y + item.height) &&
-  //               (this.y + this.height > item.y);
-  //     }
-    
-  
       draw(){
+        this.x-=4;
         ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
       }
   }
 
+  class Bike{
+    constructor(x,y,img){
+        this.x = x; 
+        this.y = y; 
+        this.width = 64;
+        this.height = 64;
+        this.image = new Image();
+        this.image.src = img;
+        this.image.onload = function(){
+            this.draw();
+        }.bind(this);
+  
+    }
+  
+      draw(){
+        this.x+=2;
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+      }
+  }
+
+  class Trajinera{
+    constructor(x,y,img){
+        this.x = x; 
+        this.y = y; 
+        this.width = 80;
+        this.height = 80;
+        this.image = new Image();
+        this.image.src = img;
+        this.image.onload = function(){
+            this.draw();
+        }.bind(this);
+  
+    }
+    
+      draw(){
+        this.x+=1;
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+      }
+  }
+
+  class Axolotl{
+    constructor(x,y,img){
+        this.x = x; 
+        this.y = y; 
+        this.width = 64;
+        this.height = 64;
+        this.image = new Image();
+        this.image.src = img;
+        this.image.onload = function(){
+            this.draw();
+        }.bind(this);
+  
+    }
+  
+      draw(){
+        this.x-=1;
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+      }
+  }
 
 //instances
 var board = new Board();
 var xolo = new Xolo();
-var taxi = new Car();
 
 //main functions
 function update(){
     frames++;
     ctx.clearRect(0,0,canvas.width,canvas.height);
     board.draw();
+    generateTrajineras();
+    drawTrajineras();
+    generateAxolotls();
+    drawAxolotls();
     xolo.draw();
-    taxi.draw();
-    // generatePipes();
-    // drawPipes();
+    generateTaxis();
+    drawTaxis();
+    generateTrucks();
+    drawTrucks();
+    generateTamaleros();
+    drawTamaleros();
   }
   
   function start(){
@@ -169,9 +254,103 @@ function update(){
     sound.play();
   }
   
+
+//aux functions
+
+function generateTaxis(){
+    if(!(frames%110===0) ) return;
+    var taxi = new Car(canvas.width, canvas.height - 128, images.car);
+    taxis.push(taxi);
+}
+
+function drawTaxis(){
+    taxis.forEach(function(taxi){
+        taxi.draw();
+        if(xolo.isTouching(taxi)){
+            xoloDies();
+        }
+    })
+}
+
+function generateTrucks(){
+    if(!(frames%120===0) ) return;
+    var truck = new Truck(canvas.width, canvas.height - 192, images.truck);
+    trucks.push(truck);
+}
+
+function drawTrucks(){
+    trucks.forEach(function(truck){
+        truck.draw();
+        if(xolo.isTouching(truck)){
+            xoloDies();
+        }
+    })
+}
+
+function generateTamaleros(){
+    if(!(frames%140===0) ) return;
+    var tamalero = new Bike(-64, canvas.height - 256, images.cyclist);
+    tamaleros.push(tamalero);
+}
+
+function drawTamaleros(){
+    tamaleros.forEach(function(tamalero){
+        tamalero.draw();
+        if(xolo.isTouching(tamalero)){
+            xoloDies();
+        }
+    })
+}
+
+function generateTrajineras(){
+    if(!(frames%190===0) ) return;
+    var trajinera = new Trajinera(-64, canvas.height - 330, images.trajinera);
+    trajineras.push(trajinera);
+}
+
+function drawTrajineras(){
+    trajineras.forEach(function(trajinera){
+        trajinera.draw();
+    })
+}
+
+function generateAxolotls(){
+    if(!(frames%120===0) ) return;
+    var axolotl = new Axolotl(canvas.width, canvas.height - 364, images.axolotl);
+    axolotls.push(axolotl);
+}
+
+function drawAxolotls(){
+    axolotls.forEach(function(axolotl){
+        axolotl.draw();
+    })
+}
+
+function xoloDies(){
+    clearInterval(interval);
+    interval = undefined;
+    sound.pause();
+    sound.currentTime = 0;
+    lives--;
+    restart();
+    if (lives===0) {
+        board.gameOver();
+    }
+}
+
+function restart(){
+    if(interval) return;
+    pipes = [];
+    frames = 0;
+    flappy.x = 100;
+    flappy.y = 100;
+    start();
+}
+
+  
 //event listeners
 
-
+start();
 
 addEventListener('keydown', function(e){
     switch(e.keyCode){      
